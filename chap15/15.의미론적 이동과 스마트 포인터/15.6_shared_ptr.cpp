8423eaea7d5b952cda07266d04a965b7d3da09dc
2020_c++_project1 <haebin1223.myk@gmail.com>
2020-04-08 23:34:16 +0900

    15.6-1

--- chap15/15.의미론적 이동과 스마트 포인터/15.6_shared_ptr.cpp
+++ chap15/15.의미론적 이동과 스마트 포인터/15.6_shared_ptr.cpp
@@ -8,5 +8,26 @@ using namespace std;
 
 int main()
 {
-   
+    //1. Resource를 포인터로 만들고, shared pointer를 ptr 포인터에 넣으면서 초기화 하는 방법 
+	Resource *res = new Resource(3);
+	res->setAll(1);
+	shared_ptr<Resource> ptr1(res);
+
+	//2. make_shared 사용 
+	//auto ptr1 = make_shared<Resource>(3);
+	//ptr1->setAll(1)
+
+	ptr1->print();
+
+
+	{
+		shared_ptr<Resource> ptr2(ptr1);
+
+		ptr2->setAll(3);
+		ptr2->print();
+
+		cout << "Going out of the block" << endl;
+
+
+	}
 }
